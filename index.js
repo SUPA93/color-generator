e4a347483bb455a71bb3475965463dd76a1d8665
SUPA93 <supaflavaz93@hotmail.com>
2024-04-16 18:12:49 +0200

    added timer and function.

--- index.js
+++ index.js
@@ -1,8 +1,19 @@
-const random1 = Math.floor(Math.random() * 255);
-const random2 = Math.floor(Math.random() * 255);
-const random3 = Math.floor(Math.random() * 255);
 
-const color = `rgb(${random1}, ${random2}, ${random3})`;
+function getColor(){
 
-document.body.style.background = color;
-document.body.innerHTML = `<h2>${color}</h2>`;
+    const random1 = Math.floor(Math.random() * 255);
+    const random2 = Math.floor(Math.random() * 255);
+    const random3 = Math.floor(Math.random() * 255);
+
+
+    const color = `rgb(${random1}, ${random2}, ${random3})`;
+    const color2 = `rgb(${random2}, ${random3}, ${random1})`;
+
+    document.body.style.background = color;
+    document.body.innerHTML = `<h2>${color}</h2>`;
+
+    const h2 = document.querySelector('h2');
+    h2.style.border = `5px solid ${color2}`;
+};
+
+setInterval(getColor, 3000);
